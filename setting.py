36e0295be68f2cfe2e2677783f1b08a5cf72f01d
alexech1989 <danielechevarria199@gmail.com>
2013-06-18 09:07:43 -0500

    Update setting.py

--- setting.py
+++ setting.py
@@ -5,7 +5,6 @@
         configurables utilizados por el resto de modulos del script
 
     Desarrollado por: Daniel Echevarria Iparraguirre
-        sistemas8@carolinaperu.com
 
     Fecha Creacion: 17/06/2013
 
